ed7c67b2c5a1e674def01a16535d472c6b31e518
Animesh Karmakar <animeshkarmakar992@gmail.com>
2026-01-16 21:42:43 +0530

    cleanup

--- _archive_cleanup_2026-01-14/check_data_raw.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_csv("d:/Major/Backend/supplier_intelligence/processed_supplier_data.csv")
-summary = df.groupby('supplier').agg({
-    'delay_days': 'mean',
-    'rejection_ratio': 'mean',
-    'fulfillment_ratio': 'mean'
-}).round(2)
-print(summary.loc[["Alpha Parts", "Nova Logistics", "Apex Logistics", "Mega Corp"]])
